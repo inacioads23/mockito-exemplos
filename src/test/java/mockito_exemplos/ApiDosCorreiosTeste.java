735748dfedd0558aa4b44c6e1b89ff4ca7cf075d
inacioads23 <jinacio11@gmail.com>
2025-07-21 12:04:43 -0300

    add examples tests

---
+++ src/test/java/mockito_exemplos/ApiDosCorreiosTeste.java
@@ -0,0 +1,39 @@
+package mockito_exemplos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Teste da classe {@link ApiDosCorreios} onde é apresentado os matchers com maior detalhes
+ */
+@ExtendWith(MockitoExtension.class)
+public class ApiDosCorreiosTeste {
+
+    @Mock
+    private ApiDosCorreios apiDosCorreios;
+
+    @Test
+    void retornaObjetoQuandoEspecificoValorEhPassado() {
+        when(apiDosCorreios.buscaDadosComBaseNoCep(anyString())).thenReturn(null);
+
+        DadosLocalizacao dadosLocalizacao = apiDosCorreios.buscaDadosComBaseNoCep("161761");
+
+        assertNull(dadosLocalizacao);
+    }
+
+    @Test
+    void retornaObjetoQuandoEspecificoValorEhPassado2() {
+        when(apiDosCorreios.buscaDadosComBaseNoCep("78787878")).thenReturn(new DadosLocalizacao("AM", "Manaus", "Rua Grajaúna", "casa", "Lago Azul"));
+
+        DadosLocalizacao dadosLocalizacao = apiDosCorreios.buscaDadosComBaseNoCep("78787878");
+
+        assertEquals("AM", dadosLocalizacao.getUf());
+    }
+}
---
+++ src/test/java/mockito_exemplos/EnviarMensagemTeste.java
@@ -0,0 +1,31 @@
+package mockito_exemplos;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Teste da classe {@link EnviarMensagem} exemplificando Spy
+ */
+@ExtendWith(MockitoExtension.class)
+public class EnviarMensagemTeste {
+
+    @Spy
+    EnviarMensagem enviarMensagem = new EnviarMensagem();
+
+    @Test
+    void adicionarMensagem() {
+        Mensagem mensagem = new Mensagem("Hello World");
+
+        enviarMensagem.adicionarMensagem(mensagem);
+
+        verify(enviarMensagem).adicionarMensagem(mensagem);
+
+        Assertions.assertEquals(1, enviarMensagem.getMensagens().size());
+    }
+
+}
