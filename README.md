d4837f85c9045c613dff9db8f7425bc5da9bdc3c
inacioads23 <jinacio11@gmail.com>
2025-07-21 10:36:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mockito-exemplos
+Exemplo de utilização do Mockito em projetos Java
+
+- https://www.baeldung.com/mockito-spy
---
+++ src/main/java/mockito_exemplos/App.java
@@ -0,0 +1,13 @@
+package mockito_exemplos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
